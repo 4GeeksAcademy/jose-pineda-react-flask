548b16918ca2535bb3a8e32c1ebfa4d02d478df0
Joeyy485 <125569895+Joeyy485@users.noreply.github.com>
2023-07-19 22:48:03 +0000

    Added signup route

--- src/api/routes.py
+++ src/api/routes.py
@@ -22,3 +22,25 @@ def login():
 
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
+
+@api.route("/signup", methods=["POST"])
+def signup():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+
+    
+    if not email or not password:
+        raise APIException("Email and password are required fields.", status_code=400)
+
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        raise APIException("User with this email already exists.", status_code=400)
+
+    # Create a new user and save it to the database
+    new_user = User(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully."}), 201
